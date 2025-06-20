454838cb528fa0aa3e8627a09054e5bcb4b19e78
= <developergurru@gmail.com>
2025-06-20 15:20:56 +0500

    env fixed

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,10 @@
-import dotenv from "dotenv";
-
-export const config = dotenv.config().parsed || {};
+export const config = {
+    Domain : import.meta.env.VITE_DOMAIN,
+    Environment: import.meta.env.VITE_ENVIRONMENT || "production",
+    Url : import.meta.env.VITE_URL || "https://www.userbird.com",
+    LogoUrl: import.meta.env.VITE_LOGOURL || "https://www.userbird.com/logo.svg",
+    LogoWidth: import.meta.env.VITE_LOGOWIDTH || "24px",
+    LogoHeight: import.meta.env.VITE_LOGOHEIGHT || "24px",  
+    LogoAlt: import.meta.env.VITE_LOGOALT || "UserBird Logo",
+    LocalHost: import.meta.env.VITE_LOCALHOST,
+};
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+import { defineConfig } from 'vite';
+
+export const emv = () => defineConfig({
+  define: {
+    'import.meta.env.VITE_URL': JSON.stringify(process.env.VITE_URL || 'https://default.url.com')
+  },
+});
+
 export default {
   build: {
     lib: {
